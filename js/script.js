3cafb6d055d9cd66f20fcb45fa6ca46b647470ab
andreyl79 <alyaginskow@gmail.com>
2016-09-19 22:11:28 +0300

    new projects slides and mobile menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 (function($){
 	function indexbg (){
-		var $wH, $headerContentBg, $headerBgH;
+		var $wH, $headerContentBg, $headerBgH, $addressBar;
+		$addressBar = $('.header__address-bar');
+		$addresBarH = $addressBar.outerHeight();
+		console.log($addresBarH);
 		$wH = $(window).height();
 		$headerContentBg = $('.header-content');
-		$headerBgH = $headerContentBg.height($wH - 30);		
+		$headerBgH = $headerContentBg.height($wH - $addresBarH);
 	};
 
 	function readmore (){
@@ -16,11 +19,11 @@
 		console.log($sectionAboutH);
 		$readMoreButt.on('click', function(){
 			totalHeigth = 0;
-			$sectionElements = $sectionAboutContent.children(); 
+			$sectionElements = $sectionAboutContent.children(':not(.more)');
 			$sectionElements.each(function(){
-			totalHeigth += $(this).outerHeight();
+			totalHeigth += $(this).outerHeight(true);
 			console.log(outerHeight);
-		})
+		});
 		$sectionAboutContent
 			.css({
 				"height": $sectionAboutContent.height(),
@@ -38,12 +41,16 @@
 
 		});
 		$closeButt.on('click', function(){
-			
-			
-			$sectionAboutContent
-				.animate({
-					"height": $sectionAboutH
-				})
+			var aboutStart = $('#about-start');
+			var destination = $(aboutStart).offset().top;
+            $('html,body').animate({scrollTop: destination}, 300, function () {
+                $sectionAboutContent
+                    .animate({
+                        "height": $sectionAboutH
+                    }, 300);
+            });
+
+
 			$closeButt.fadeOut(100, function(){
 				$readMoreButt.fadeIn();
 			});
@@ -51,29 +58,48 @@
 			return false;
 		})
 	}
-			function projDescr(){
-			var projElement, elementImg, elementDescr;
-			projElement = $('.element');
-			elementImg = $('.element-img');
-			elementDescr = $('.element-descr');
-			projElement.click(function(){
-				projElement.not(this)
-					.removeClass('element--active')
-					.css('z-index','90')
-					.find(elementDescr)
-					.removeClass('descr-active');
+    function projDescr(){
+        var item;
+        item = $('.item');
+        item.on('click', function (evt) {
+            var $this, $items, $target;
+            $this = $(this);
+            $items = $('.item');
+            $target = $(evt.target);
 
-				$(this).toggleClass('element--active')
-					.css('z-index','')
-					.find(elementDescr)
-					.toggleClass('descr-active');
-			});
-		};
+            evt.preventDefault();
+            evt.stopPropagation();
+            $this.parent().toggleClass('active');
+            $this.toggleClass('js--show');
+            $items.not(this).removeClass('js--show').parent().removeClass('active');
+        })
+    }
+
+    function nav() {
+        var $menuButton, $sideNav, $sideNavMask;
+
+        $menuButton = $('#menu-reveal');
+        $sideNav = $('#mobile-header-menu__contacts');
+        $sideNavMask = $('#side-nav-mask');
+
+        $menuButton.on('click', function() {
+            $sideNav.addClass('visible');
+            $sideNavMask.addClass('visible');
+			$("html, body").css('position','fixed');
+        });
+
+        $sideNavMask.on('click', function() {
+            $sideNav.removeClass('visible');
+            $sideNavMask.removeClass('visible');
+			$("html, body").css('position','');
+        });
+    }
 
 
 $(document).ready(function(){
 	indexbg();
 	readmore();
 	projDescr();
+    nav();
 })
 })(jQuery)
